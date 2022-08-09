638bfa1882d0667752ffd468bc807ed21d72f240
Sriparno Ganguly <89468039+K2E7@users.noreply.github.com>
2022-08-09 21:17:59 +0530

    committed first.java

---
+++ Special Assignment/002_User_Defined/first.java
@@ -0,0 +1,13 @@
+package PACK1;
+
+import PACK1.PACK2.second;
+public class first
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello this is the first class");
+
+        second p = new second();
+        p.f();
+    }
+}
