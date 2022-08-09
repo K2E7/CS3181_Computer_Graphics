10aabbb8929786fb714b3aaea9c27c7231a720b7
Sriparno Ganguly <89468039+K2E7@users.noreply.github.com>
2022-08-09 21:22:17 +0530

    committed second_derived.java

---
+++ Special Assignment/004_Inheritance/second_derived.java
@@ -0,0 +1,13 @@
+package PACK1.PACK2;
+import PACK1.PACK2.second;
+public class second_derived extends second{
+    public void fd()
+    {
+        System.out.println("Okay this is derived.");
+    }
+
+    public void f()
+    {
+        System.out.println("This is me changing the second class.");
+    }
+}
