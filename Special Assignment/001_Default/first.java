ed1d420101eada3668e3aace02965496a67d5455
Sriparno Ganguly <89468039+K2E7@users.noreply.github.com>
2022-08-09 21:16:33 +0530

    commited first.java

---
+++ Special Assignment/001_Default/first.java
@@ -0,0 +1,9 @@
+class first
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello World, I am first.");
+        second p = new second();
+        p.f();
+    }
+}
