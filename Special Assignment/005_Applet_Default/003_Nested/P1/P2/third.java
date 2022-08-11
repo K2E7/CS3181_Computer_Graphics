a2ff60ca3890f397e5d6b65de89eb67151071aaf
Sriparno Ganguly <89468039+K2E7@users.noreply.github.com>
2022-08-11 22:43:13 +0530

    third.java

---
+++ Special Assignment/005_Applet_Default/003_Nested/P1/P2/third.java
@@ -0,0 +1,13 @@
+package P1.P2;
+import java.awt.*;
+
+public class third 
+{
+    Font F2 = new Font("Arial",Font.BOLD,20);
+    public void draw_third(Graphics G)
+    {
+        G.setFont(F2);
+        G.setColor(Color.black);
+        G.drawString("I hope this is visible too",130,190);
+    }
+}
