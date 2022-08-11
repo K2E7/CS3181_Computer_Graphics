4e92a1c999046e9a598c2c8ffaf3116c2d3559ab
Sriparno Ganguly <89468039+K2E7@users.noreply.github.com>
2022-08-11 22:42:49 +0530

    second.java

---
+++ Special Assignment/005_Applet_Default/003_Nested/P1/second.java
@@ -0,0 +1,12 @@
+package P1;
+import java.awt.*;
+
+public class second 
+{   
+    
+    public void draw_second(Graphics G)
+    {
+        G.setColor(Color.lightGray);
+        G.fillRect(100,100,400,300);
+    }
+}
