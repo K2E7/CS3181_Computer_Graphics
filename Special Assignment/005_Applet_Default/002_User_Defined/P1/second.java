c5b9e846075445ad877a43b7fbfccfb12783ae53
Sriparno Ganguly <89468039+K2E7@users.noreply.github.com>
2022-08-11 22:41:28 +0530

    second.java

---
+++ Special Assignment/005_Applet_Default/002_User_Defined/P1/second.java
@@ -0,0 +1,12 @@
+package P1;
+
+import java.awt.*;
+
+public class second
+{
+    public void draw(Graphics G)
+    {
+        G.setColor(Color.LIGHT_GRAY);
+        G.fillRect(100,100,400,300);
+    }
+}
